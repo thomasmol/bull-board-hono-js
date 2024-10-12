d53bb1993ac7e4705e9456aee34b94e9a313dc14
Thomas Mol <email@thomasmol.com>
2024-10-12 12:29:39 +0200

    Refactor Redis connection URL in src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ import { v4 as uuidv4 } from 'uuid';
 
 dotenv.config();
 const port = process.env.PORT || 3000;
-const redis = new Redis(`${process.env.PRIVATE_REDIS_URL}?family=0`, {
+const redis = new Redis(`${process.env.PRIVATE_REDIS_URL}`, {
   maxRetriesPerRequest: null,
 });
 const createQueueMQ = (name: string) =>
